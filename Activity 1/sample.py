998c42a1e0ea87be4a79a1564f47ef6ebb039933
Hunya-syete <tinangmarcillones@gmail.com>
2024-09-03 09:14:20 +0800

    Activity 4

---
+++ Activity 1/sample.py
@@ -0,0 +1,28 @@
+import math
+
+grade_1 = 95.656
+grade_2 = 85.68561
+
+print(round(grade_1, 2))
+print(math.ceil(grade_1))
+print(math.floor(grade_2))
+
+
+
+# a = int(input("Enter you first number: "))
+# b = int(input("Enter you second number: "))
+# c = a + b
+# print("Your value is: ", c)
+
+
+
+
+
+# first_name = input("Enter first name: ")
+# last_name = input("Enter last name: ")
+# age = input("Enter age: ")
+# print("Your last name is ", last_name)
+# print("Your first name is ", first_name)
+# print("Your age is ", age)
+
+
---
+++ Activity 4/Activity 4.py
@@ -0,0 +1,8 @@
+
+sampleText2 = "My name is {2}. I love {1}. I love playing {0}."
+sampleText2a = sampleText2.format("Christine", "Burger", "Mobile Legends")
+print(sampleText2a)
+
+sampleText3 = "My name is {name}. I love {food}. I love playing {play}."
+sampleText3a = sampleText3.format(name="Christine", food="Burger", play="Mobile Legends")
+print(sampleText3a)
