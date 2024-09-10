785e8c819bf88818bcf036316c509f7109efa3d6
shaishanchay <shainaacurin0@gmail.com>
2024-09-10 09:35:21 +0800

    Update acurin.py

--- Activity 4/acurin.py
+++ Activity 4/acurin.py
@@ -17,3 +17,40 @@ sampleText4 = ("the product %s is cost %.2f" % (item, cost))
 print(sampleText4)
 
 The product is milk the cost 35.50,
+
+# item = "milk"
+# cost = 35.5
+# SampleText4 = "The product is %s the cost is %.5f." % (item, cost)
+# print(SampleText4)
+
+item = "apple watch"
+cost = 9900
+SampleText5 = f"The item is {item} and the cost is {cost * 100} pesos."
+print(SampleText5)  # Output: The item is apple watch and the cost is
+
+The item is apple watch and the cost is 990000 pesos.
+
+# 9
+strings = ["Animals", "Badger", "Honey Bee", "Honey Badger"]
+
+for string in strings:
+    lowercase_string = string.lower()
+    print(lowercase_string)
+
+animals
+badger
+honey bee
+honey badger
+
+# 10
+strings = ["Animals", "Badger", "Honey Bee", "Honey Badger"]
+
+for string in strings:
+    uppercase_string = string.upper()
+    print(uppercase_string)
+
+ANIMALS
+BADGER
+HONEY BEE
+HONEY BADGER
+
