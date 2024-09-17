7bdc65a29b7999465775cf697c348dc22a4b6e61
beautyandthebees <belleelentorio27@gmail.com>
2024-09-17 09:41:24 +0800

    Create elentorio.py

---
+++ Activity 6/elentorio.py
@@ -0,0 +1,21 @@
+name = input("Enter your name: ")
+age = input("Enter your age: ")
+# Use f-string to format the output
+print(f"Hello, {name} You are {age} years old. ")
+
+item = "banana"
+count = 7
+# Use the .format() method
+sentence = "I bought {} {} today. " .format(count, item)
+print(sentence)
+
+city = "Paris"
+temperature = 24
+# Use the % operator
+print("The temperature in %s is %d degrees celcius." %(city, temperature))
+
+Enter your name: belle
+Enter your age: 18
+Hello, belle You are 18 years old. 
+I bought 7 banana today. 
+The temperature in Paris is 24 degrees celcius.
